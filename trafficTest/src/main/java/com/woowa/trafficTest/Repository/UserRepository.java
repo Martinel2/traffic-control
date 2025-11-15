b39ebae8cc02703ece1e8fff29e0a459076af349
Martinel2 <kkuldangi3@naver.com>
2025-11-15 17:32:26 +0900

    fix: 코드 최적화하기
    - N+1문제를 해결하기 위해 모두 가져오는 방식을 채택
    - 하지만, count가 long값이라 추가적인 수정이 필요
    - gc최적화를 위해 List에서 StringBuilder로 로그를 받도록 함

--- trafficTest/src/main/java/com/woowa/trafficTest/Repository/UserRepository.java
+++ trafficTest/src/main/java/com/woowa/trafficTest/Repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.woowa.trafficTest.Repository;
 import com.woowa.trafficTest.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByOrderByIdAsc();
 }
--- trafficTest/src/main/java/com/woowa/trafficTest/Service/UserService.java
+++ trafficTest/src/main/java/com/woowa/trafficTest/Service/UserService.java
@@ -17,18 +17,16 @@ public class UserService {
 
     public int processUsers(int count) {
         List<User> users = new ArrayList<>();
-        List<String> garbageLog = new ArrayList<>(); // L1(GC) 문제를 일으킬 리스트
+        StringBuilder garbageLog = new StringBuilder(); // L1(GC) 문제를 일으킬 리스트
+        List<User> usersBefore = userRepository.findAllByOrderByIdAsc();
 
         for (long i = 1; i <= count; i++) {
-            // [L2 문제]: 루프 안에서 N+1 쿼리 발생
-            User user = userRepository.findById(i).orElse(null);
+            User user = usersBefore.get((int) i);
 
             if (user != null) {
                 users.add(user);
 
-                // [L1 문제]: 매번 불필요한 객체를 생성하여 GC 유발
-                String log = user.makeLog();
-                garbageLog.add(log);
+                garbageLog.append(user.makeLog()).append("\n");
             }
         }
         return users.size();
