f0ab116a6f3c0cc09fdf09eecb0a7d8c4d405a5f
Martinel2 <kkuldangi3@naver.com>
2025-11-16 22:14:01 +0900

    refactor: Service코드 수정 및 README 업데이트
    - 코드를 분할 처리 시스템으로 재구성
    - Readme에 수정 후 실행결과 업데이트

--- trafficTest/src/main/java/com/woowa/trafficTest/Service/UserService.java
+++ trafficTest/src/main/java/com/woowa/trafficTest/Service/UserService.java
@@ -3,32 +3,50 @@ package com.woowa.trafficTest.Service;
 import com.woowa.trafficTest.Entity.User;
 import com.woowa.trafficTest.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
+    private static final Logger userLogger = LoggerFactory.getLogger("USER_LOG");
 
     public int processUsers(int count) {
-        List<User> users = new ArrayList<>();
-        StringBuilder garbageLog = new StringBuilder(); // L1(GC) 문제를 일으킬 리스트
-        List<User> usersBefore = userRepository.findAllByOrderByIdAsc();
+        int total = 0;
+        long lastId = 1L;
 
-        for (long i = 1; i <= count; i++) {
-            User user = usersBefore.get((int) i);
-
-            if (user != null) {
-                users.add(user);
-
-                garbageLog.append(user.makeLog()).append("\n");
+        while (total < count) {
+            List<User> pageUsers = getUsers(count, total, lastId);
+            if (pageUsers.isEmpty()) {
+                break;
             }
+
+            processLog(pageUsers);
+            lastId += pageUsers.size();
+            total += pageUsers.size();
         }
-        return users.size();
+
+        return total;
+    }
+
+    private void processLog(List<User> users) {
+        for (User user : users) {
+            userLogger.info(user.makeLog());
+        }
+    }
+
+    private List<User> getUsers(int count, int totalProcessedCount, long lastSeenId) {
+        int remainingCount = count - totalProcessedCount;
+        int MAX_PROCESS_SIZE = 25;
+        int currentChunkSize = Math.min(MAX_PROCESS_SIZE, remainingCount);
+
+        Pageable pageable = PageRequest.of(0, currentChunkSize);
+        return userRepository.findByIdGreaterThanOrderByIdAsc(lastSeenId, pageable);
     }
 }
